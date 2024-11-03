44e55f8acb565aa4a26f2068c11d20453add87cf
Accany1 <129660499+Accany1@users.noreply.github.com>
2024-11-03 15:59:59 +0800

    removed html and added mergesort

---
+++ merge.js
@@ -0,0 +1,33 @@
+const mergeSort = (arr) => {
+  if (arr.length <= 1) return arr;
+  const middle = Math.floor(arr.length / 2);
+  const left = arr.slice(0, middle);
+  const right = arr.slice(middle);
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+merge = (arr1, arr2) => {
+  const result = [];
+  let i = 0;
+  let j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      result.push(arr1[i]);
+      i++;
+    } else {
+      result.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    result.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    result.push(arr2[j]);
+    j++;
+  }
+  return result;
+};
+
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]));
